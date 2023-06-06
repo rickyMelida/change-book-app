6b90669f9c7208d489e779ad4e20e262c2503ff6
Ricardo Melida <ricardomelida92@gmail.com>
2023-06-06 12:07:12 -0400

    Se modifica el tiempo de expiracion de la cookie

--- src/hooks/setCookie.js
+++ src/hooks/setCookie.js
@@ -2,9 +2,9 @@ import Cookie from 'js-cookie';
 
 const setCookie = (cookieName, cookieValue) => {
 	const inAnHour = 1 / 24;
-	
+
 	Cookie.set(cookieName, cookieValue, {
-		expires: 1 / inAnHour,
+		expires: inAnHour,
 		secure: true,
 		sameSite: 'strict',
 		path: '/',
